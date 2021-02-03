9a63c40425899eb28ab1340d5cea4b1636c80aaf
gchopin <gchopin@42.student.fr>
2021-02-03 15:53:53 +0100

    ajout fichier bmp

--- srcs/bmp/bmp.c
+++ srcs/bmp/bmp.c
@@ -2,7 +2,7 @@
 
 int	ft_open_bmp(t_map *map, char *path)
 {
-	int	fd;
+	int		fd;
 	char	*bmp_file;
 	char	*substr;
 
@@ -10,7 +10,7 @@ int	ft_open_bmp(t_map *map, char *path)
 	bmp_file = ft_strjoin(substr, ".bmp");
 	if (bmp_file == NULL)
 		close_program(map, "Can't get name bmp.\n", 2);
-	fd = open(bmp_file, O_RDWR | O_CREAT, 0777);
+	fd = open(bmp_file, O_CREAT | O_RDWR, 0777);
 	if (substr)
 		free(substr);
 	if (bmp_file)
@@ -21,26 +21,91 @@ int	ft_open_bmp(t_map *map, char *path)
 	}
 	return (fd);
 }
+
+void	write_header(t_map *map, int fd)
+{
+	unsigned short		two_bytes;
+	unsigned int		four_bytes;
+	int	s_four_bytes;
+
+	four_bytes = 54 + (map->res_x * map->res_y);
+	s_four_bytes = 0;
+	two_bytes = 0;
+	ft_putstr_fd("BM", fd);
+	write(fd, &four_bytes, sizeof(four_bytes));
+	write(fd, &two_bytes, sizeof(unsigned short));
+	write(fd, &two_bytes, sizeof(unsigned short));
+	four_bytes = 54;
+	write(fd, &four_bytes, sizeof(four_bytes));
+}
+
+void	write_header_img(t_map *map, int fd)
+{
+	unsigned short		two_bytes;
+	unsigned int		four_bytes;
+	int		s_four_bytes;
+
+	four_bytes = 40;
+	write(fd, &four_bytes, sizeof(unsigned int));
+	write(fd, &map->res_x, sizeof(signed int));
+	write(fd, &map->res_y, sizeof(signed int));
+	two_bytes = 1;
+	write(fd, &two_bytes, sizeof(unsigned short));
+	two_bytes = (unsigned short)map->image[0].bpp;
+	write(fd, &two_bytes, sizeof(unsigned short));
+	four_bytes = 0;
+	write(fd, &four_bytes, sizeof(unsigned int));
+	write(fd, &four_bytes, sizeof(unsigned int));
+	write(fd, &s_four_bytes, sizeof(signed int));
+	write(fd, &s_four_bytes, sizeof(signed int));
+	write(fd, &four_bytes, sizeof(unsigned int));
+	write(fd, &four_bytes, sizeof(unsigned int));
+}
+
+void	write_img(t_map *map, int fd)
+{
+	int	x;
+	int	y;
+	int	pixel;
+
+	x = 0;
+	y = map->res_y;
+	pixel = 0;
+	while (y > 0)
+	{
+		x = 0;
+		y--;
+		while (map->res_x > x)
+		{
+			pixel = (y * map->image[0].line_bytes) + (x * 4);
+			write(fd, &map->image[0].mlx_get_data[pixel], sizeof(int));
+			x++;
+		}
+	}
+}
+
 /*
- * filesize = taille header + resolution d ecran (nb pixel)
+ ** Source : https://medium.com/sysf/bits-to-bitmaps-a-simple-walkthrough-of-bmp-image-format-765dc6857393
 */
+
 void	create_bmp(t_map *map, char *argv)
 {
-	int		fd;
-	unsigned int	filesize;
+	int	fd;
 
 	fd = ft_open_bmp(map, argv);
-	filesize = 54 + (map->res_x * map->res_y);
 	if (fd != 1)
 	{
-		ft_putstr_fd("BM", fd);
-		write(fd, &filesize, sizeof(filesize));
-		ft_putchar_fd((unsigned)0, fd);
-		ft_putchar_fd((unsigned)0, fd);
-		ft_putchar_fd((unsigned)0, fd);
-		ft_putchar_fd((unsigned)0, fd);
-		filesize = 54;
-		write(fd, &filesize, sizeof(filesize));
+		if (map->image[0].mlx_get_data != NULL)
+		{
+			write_header(map, fd);
+			write_header_img(map, fd);
+			write_img(map, fd);
+		}
+		else
+		{
+			close_program(map, "Can't take screenshot\n", 2);
+			ft_close_fd(fd);
+		}
 	}
 	if (fd != -1)
 	{
