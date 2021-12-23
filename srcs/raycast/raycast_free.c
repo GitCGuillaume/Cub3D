5fc7dffcf07d33146a6960a171beb2d1eb78f038
Guillaume CHOPIN <gchopin@e2r12p21.clusters.42paris.fr>
2021-12-23 18:14:57 +0100

    push

--- srcs/raycast/raycast_free.c
+++ srcs/raycast/raycast_free.c
@@ -2,11 +2,11 @@
 
 void	check_error_ln_hor(t_map *map, double floor_pos_y)
 {
-	if ((floor_pos_y <= -2147483647.0 || floor_pos_y >= 2147483647.0)
-			|| (map->player.ray_horizontal.length_case_x <= -2147483647.0
-				|| map->player.ray_horizontal.length_case_x >= 2147483647.0)
-		|| (map->player.ray_horizontal.length_case_y <= -2147483647.0
-				|| map->player.ray_horizontal.length_case_y >= 2147483647.0))
+	if ((floor_pos_y <= -2147483647.000000 || floor_pos_y >= 2147483647.000000)
+			|| (map->player.ray_horizontal.length_case_x <= -2147483647.000000
+				|| map->player.ray_horizontal.length_case_x >= 2147483647.000000)
+		|| (map->player.ray_horizontal.length_case_y <= -2147483647.000000
+				|| map->player.ray_horizontal.length_case_y >= 2147483647.000000))
 	{
 		close_program(map, "horizontal ray casting is wrong.\n", 2);
 	}
@@ -14,11 +14,11 @@ void	check_error_ln_hor(t_map *map, double floor_pos_y)
 
 void	check_error_ln_ver(t_map *map, double floor_pos_x)
 {
-	if ((floor_pos_x <= -2147483647.0 || floor_pos_x >= 2147483647.0)
-			|| (map->player.ray_vertical.length_case_x <= -2147483647.0
-				|| map->player.ray_vertical.length_case_x >= 2147483647.0)
-		|| (map->player.ray_vertical.length_case_y <= -2147483647.0
-				|| map->player.ray_vertical.length_case_y >= 2147483647.0))
+	if ((floor_pos_x <= -2147483647.000000 || floor_pos_x >= 2147483647.000000)
+			|| (map->player.ray_vertical.length_case_x <= -2147483647.000000
+				|| map->player.ray_vertical.length_case_x >= 2147483647.000000)
+		|| (map->player.ray_vertical.length_case_y <= -2147483647.000000
+				|| map->player.ray_vertical.length_case_y >= 2147483647.000000))
 	{
 		close_program(map, "vertical ray casting is wrong.\n", 2);
 	}
@@ -26,8 +26,8 @@ void	check_error_ln_ver(t_map *map, double floor_pos_x)
 
 void	check_loop_cast_hor(t_map *map)
 {
-	if (map->player.ray_horizontal.length_case_x >= 2147483647.0
-		|| map->player.ray_horizontal.length_case_y >= 2147483647.0)
+	if (map->player.ray_horizontal.length_case_x >= 2147483647.000000
+		|| map->player.ray_horizontal.length_case_y >= 2147483647.000000)
 	{
 		close_program(map,
 				"horizontal ray casting values loop are wrong.\n", 2);
@@ -36,8 +36,8 @@ void	check_loop_cast_hor(t_map *map)
 
 void	check_loop_cast_ver(t_map *map)
 {
-	if (map->player.ray_vertical.length_case_x >= 2147483647.0
-		|| map->player.ray_vertical.length_case_y >= 2147483647.0)
+	if (map->player.ray_vertical.length_case_x >= 2147483647.000000
+		|| map->player.ray_vertical.length_case_y >= 2147483647.000000)
 	{
 		close_program(map, "vertical ray casting values loop are wrong.\n", 2);
 	}
@@ -45,8 +45,8 @@ void	check_loop_cast_ver(t_map *map)
 
 void	check_distance(t_map *map)
 {
-	if (map->player.ray_horizontal.distance_wall > 2147483646.0
-			|| map->player.ray_vertical.distance_wall > 2147483646.0)
+	if (map->player.ray_horizontal.distance_wall > 2147483646.000000
+			|| map->player.ray_vertical.distance_wall > 2147483646.000000)
 	{
 		close_program(map, "distances values are wrong.\n", 2);
 	}
